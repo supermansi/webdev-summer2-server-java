8bc976cd6f259e5afae588633b00ec97cbe87a85
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-10 19:34:14 -0400

    index file

---
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -0,0 +1,10 @@
+(function(){
+	var $username, $firstName;
+	var $updateBtn;
+	var $currentUser = null;
+	
+	function  init() {
+		$username = $('#username');
+		$firstName = $('#firstName');
+	}
+})();
