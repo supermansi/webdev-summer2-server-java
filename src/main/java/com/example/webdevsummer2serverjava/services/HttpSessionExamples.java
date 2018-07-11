32e6975a6371a87ce9f87f095249b661347b6ea6
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-11 11:01:42 -0400

    added profile

---
+++ src/main/java/com/example/webdevsummer2serverjava/services/HttpSessionExamples.java
@@ -0,0 +1,28 @@
+package com.example.webdevsummer2serverjava.services;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class HttpSessionExamples {
+	
+	@GetMapping("/api/session/set/{attr}/{value}")
+	public String setSessionAttribute(
+			@PathVariable("attr") String attr,
+			@PathVariable("value") String value,
+			HttpSession session) {
+		session.setAttribute(attr, value);
+		return attr + " = " + value;
+	}
+	
+	@GetMapping("/api/session/get/{attr}")
+	public String getSessionAttribute(
+			@PathVariable ("attr") String attr,
+			HttpSession session) {
+		return (String) session.getAttribute(attr);
+	}
+
+
+
+}
--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -1,7 +1,38 @@
 (function(){
-	fetch('/checkLogin',{
+	
+	var $username, $firstName, $lastName;
+	
+	function init() {
+		
+		$username = $('#username');
+		$firstName = $('#firstName');
+		$lastName = $('#lastName');
+		
+		findUserById(32)
+		.then(renderUser);
+	}
+	init();
+	
+	function renderUser(user) {
+		$username.val(user.username);
+		$firstName.val(user.firstName);
+		$lastName.val(user.lastName);
+	}
+	
+	function findUserById(userId) {
+		return fetch('/api/user/' + userId)
+			.then(function(response) {
+				return response.json();
+			});
+	}
+	
+	function handleResponse() {
+		console.log(response);
+	}
+	
+	/*fetch('/checkLogin',{
 		'credentials' : 'include'
 	}).then(function(response){
 		console.log(response);
-	});
+	});*/
 })();
