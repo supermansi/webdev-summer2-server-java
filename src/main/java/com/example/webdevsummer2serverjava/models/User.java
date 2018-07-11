df18716f9d43b83b008861634bef356aca937b08
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-11 08:53:13 -0400

    modified  user repo and users

--- src/main/java/com/example/webdevsummer2serverjava/models/User.java
+++ src/main/java/com/example/webdevsummer2serverjava/models/User.java
@@ -1,11 +1,33 @@
 package com.example.webdevsummer2serverjava.models;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
 	private String username;
 	private String password;
 	private String firstName;
 	private String lastName;
+	private String phone;
+	private String email;
+	private String role;
+	private Date dateOfBirth;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	
 	public String getUsername() {
 		return username;
@@ -33,6 +55,34 @@ public class User {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
 	}
 	
 }
--- src/main/java/com/example/webdevsummer2serverjava/repositories/UserRepository.java
+++ src/main/java/com/example/webdevsummer2serverjava/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.webdevsummer2serverjava.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.webdevsummer2serverjava.models.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
+	
+	@Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
+	public User findUserByCredentials(@Param("username") String u, @Param("password") String p);
 	
 }
