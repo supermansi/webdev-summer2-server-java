085e532705a8378e2ca5281a792bf7723c430423
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-17 00:09:32 -0400

    first commit

---
+++ src/main/java/wbdv/WhiteBoardApplication.java
@@ -0,0 +1,12 @@
+package wbdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteBoardApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteBoardApplication.class, args);
+	}
+}
