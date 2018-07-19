196a6afaa60153bc0ee73a05709b530f57f6557f
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-18 21:02:13 -0400

    added module service

--- src/main/java/com/example/webdevsummer2serverjava/services/ModuleService.java
+++ src/main/java/com/example/webdevsummer2serverjava/services/ModuleService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,10 @@ public class ModuleService {
 			return course.getModules();
 		}
 		return null;
+	}
+	
+	@DeleteMapping("/api/module/{mId}")
+	public void deleteModule(@PathVariable("mId") int moduleId) {
+		moduleRepository.deleteById(moduleId);
 	}
 }
