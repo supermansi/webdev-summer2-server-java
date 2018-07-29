898b49bf34a6e12c6221c8b215e0663d125bf268
Mansi Jain <supermansi@users.noreply.github.com>
2018-07-29 14:28:26 -0400

    created widget repo

--- src/main/java/com/example/webdevsummer2serverjava/models/Widget.java
+++ src/main/java/com/example/webdevsummer2serverjava/models/Widget.java
@@ -12,16 +12,62 @@ public class Widget {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String title;
+	private String widgetType;
+	
+	// List Widget
+	private String listItems;
+	private String ordered;
+	
+	// Heading Widget
+	private int size;
+	private String text; // Paragraph Widget
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	public String getListItems() {
+		return listItems;
+	}
+	public void setListItems(String listItems) {
+		this.listItems = listItems;
+	}
+	
+	public String getOrdered() {
+		return ordered;
+	}
+	public void setOrdered(String ordered) {
+		this.ordered = ordered;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public String getWidgetType() {
+		return widgetType;
+	}
+	public void setWidgetType(String widgetType) {
+		this.widgetType = widgetType;
 	}
 }
--- src/main/java/com/example/webdevsummer2serverjava/services/WidgetService.java
+++ src/main/java/com/example/webdevsummer2serverjava/services/WidgetService.java
@@ -1,20 +1,31 @@
 package com.example.webdevsummer2serverjava.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer2serverjava.models.Widget;
+import com.example.webdevsummer2serverjava.repositories.WidgetRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class WidgetService {
+	
+	@Autowired
+	WidgetRepository widgetRepository;
 
 	@PostMapping("/api/widget")
 	public List<Widget> saveWidgets(@RequestBody List<Widget> widgets) {
-		return widgets;
+		List<Widget> savedWidgets = new ArrayList<Widget>();
+		System.out.println(widgets);
+		for(Widget widget: widgets) {
+			savedWidgets.add(widgetRepository.save(widget));
+		}
+		return savedWidgets;
 	}
 }
