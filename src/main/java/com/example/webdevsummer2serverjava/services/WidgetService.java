64d083d9d015c005946234c06219da3fd2664870
Mansi Jain <supermansi@users.noreply.github.com>
2018-08-01 20:42:03 -0400

    added REST API services

--- src/main/java/com/example/webdevsummer2serverjava/services/WidgetService.java
+++ src/main/java/com/example/webdevsummer2serverjava/services/WidgetService.java
@@ -1,7 +1,6 @@
 package com.example.webdevsummer2serverjava.services;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -71,4 +71,41 @@ public class WidgetService {
 	public void deleteWidget(@PathVariable("widgetId") int widgetId) {
 		widgetRepository.deleteById(widgetId);
 	}	
+	
+	@PutMapping("/api/widget/{widgetId}")
+	public Widget updateWidget(@PathVariable("widgetId") int widgetId,@RequestBody Widget newWidget) {
+		Optional<Widget> data= widgetRepository.findById(widgetId);
+		if(data.isPresent()) {
+			Widget widget = data.get();
+			widget.setUrl(newWidget.getUrl());
+			widget.setText(newWidget.getText());
+			widget.setOrdered(newWidget.getOrdered());
+			widget.setSize(newWidget.getSize());
+			widget.setListItems(newWidget.getListItems());
+			return widgetRepository.save(widget);
+		}
+		return null;
+	}
+	
+	@PostMapping("api/topic/{topicId}/widget")
+	public void saveAllWidgets(@PathVariable("topicId") int topicId,@RequestBody List<Widget> widgets) {
+		Optional<Topic> data=topicRepository.findById(topicId);
+		if(data.isPresent()) {
+			Topic topic =data.get();
+			List<Widget> widgetlist=topic.getWidgets();
+			for(Widget widget:widgetlist) {
+				widgetRepository.deleteById(widget.getId());
+			}
+		}
+		for(Widget widget:widgets) {
+			Optional<Topic> datatopic=topicRepository.findById(topicId);
+			if(data.isPresent()) {
+				Topic topic =datatopic.get();
+				widget.setTopic(topic);
+				widgetRepository.save(widget);
+			}
+		}
+	}
+	
+	
 }
